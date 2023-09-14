42812585026d950141c2c63dacaf5fbe7b5d5091
heesung6701 <heesung6701@gmail.com>
2023-09-14 23:54:48 +0900

    add ConfigModule

--- src/aws/s3/s3.module.ts
+++ src/aws/s3/s3.module.ts
@@ -1,7 +1,13 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { S3Repository } from "./s3.repository";
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+    }),
+  ],
   providers: [S3Repository],
   exports: [S3Repository],
 })
