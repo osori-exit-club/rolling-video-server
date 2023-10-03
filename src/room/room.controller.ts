47872ee0b1bffc24fd1b4cb609cb51e216d887c7
heesung6701 <heesung6701@gmail.com>
2023-10-03 22:52:05 +0900

    방생성 api 애노테이션 추가

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,13 +1,26 @@
 import { Controller, Get, Post, Body, Param, Delete } from "@nestjs/common";
 import { RoomService } from "./room.service";
 import { CreateRoomDto } from "./dto/create-room.dto";
+import { ApiBody, ApiOkResponse, ApiOperation } from "@nestjs/swagger";
+import { RoomDto } from "./dto/room.dto";
 
 @Controller("room")
 export class RoomController {
   constructor(private readonly roomService: RoomService) {}
 
   @Post()
-  create(@Body() createRoomDto: CreateRoomDto) {
+  @ApiOperation({
+    description: "방 생성 API",
+  })
+  @ApiBody({
+    description: "방 정보",
+    type: CreateRoomDto,
+  })
+  @ApiOkResponse({
+    description: "생성된 Room 객체",
+    type: RoomDto,
+  })
+  create(@Body() createRoomDto: CreateRoomDto): Promise<RoomDto> {
     return this.roomService.create(createRoomDto);
   }
 
