b15b6295ad7fb2dd63be64d2c628d59e9014a04c
heesung6701 <heesung6701@gmail.com>
2023-10-03 22:29:27 +0900

    roomDTO 내 Swagger 애노테이션 추가

--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -1,7 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateRoomDto {
+  @ApiProperty({
+    description: "방의 이름",
+    required: true,
+    nullable: false,
+    example: "OO의 생일파티",
+  })
   readonly name: string;
+  @ApiProperty({
+    description: "비밀번호",
+    example: "OO의 생일파티",
+  })
   readonly password: string | null;
+  @ApiProperty({
+    description: "받는 사람",
+    example: "용의자X",
+  })
   readonly recipient: string;
+  @ApiProperty({
+    description: "마감 일자",
+    example: "2023-09-17T14:41:11.487Z",
+  })
   readonly dueDate: Date;
   constructor(
     name: string,
--- src/room/dto/room.dto.ts
+++ src/room/dto/room.dto.ts
@@ -1,8 +1,32 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateRoomDto {
+  @ApiProperty({
+    description: "방의 이름",
+    required: true,
+    nullable: false,
+    example: "OO의 생일파티",
+  })
   id: string;
-  name: string;
-  password: string;
-  recipient: string;
+  readonly name: string;
+  @ApiProperty({
+    description: "비밀번호",
+    example: "OO의 생일파티",
+  })
+  readonly password: string | null;
+  @ApiProperty({
+    description: "받는 사람",
+    example: "용의자X",
+  })
+  readonly recipient: string;
+  @ApiProperty({
+    description: "마감 일자",
+    example: "2023-09-17T14:41:11.487Z",
+  })
   dueDate: Date;
+  @ApiProperty({
+    description: "Clip 리스트",
+    example: "[]",
+  })
   clipList: Record<string, string>[];
 }
