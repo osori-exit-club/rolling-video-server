e50e08c45df81394a35dba47c515588195c5239d
heesung6701 <heesung6701@gmail.com>
2023-11-05 00:06:43 +0900

    _id가 null인 경우 internal server error 발생

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -191,7 +191,7 @@ export class RoomController {
     @Body() deleteRoomDto: DeleteRoomRequest
   ): Promise<SimpleResponseDto> {
     const result = await this.roomService.remove(id, deleteRoomDto);
-    if (result != null && result._id.toString() == id) {
+    if (result && result._id && id == result._id + "") {
       return new SimpleResponseDto(ResponseMessage.ROOM_REMOVE_SUCCESS);
     }
     return new SimpleResponseDto(ResponseMessage.ROOM_REMOVE_FAIL);
