d405732527790efa4725f9a06713e6ab39c9f47b
Heesung Youn <heesung6701@naver.com>
2023-12-22 18:25:14 +0900

    ClipService 테스트 코드 수정

--- src/clip/clip.service.spec.ts
+++ src/clip/clip.service.spec.ts
@@ -1,58 +1,66 @@
-import { getModelToken } from "@nestjs/mongoose";
-import { Test, TestingModule } from "@nestjs/testing";
-import { S3Module } from "src/aws/s3/s3.module";
 import { S3Repository } from "src/aws/s3/s3.repository";
 import { RoomRepository } from "src/room/room.repository";
-import { Clip } from "src/schema/clips.schema";
-import { Room } from "src/schema/rooms.schema";
-import { HashModule } from "src/utils/hash/hash.module";
 import { ClipRepository } from "./clip.repository";
 import { ClipService } from "./clip.service";
 import { CreateClipResponse } from "./dto/response/create-clip.response.dto";
 import { CreateClipRequest } from "./dto/request/create-clip.request.dto";
 import { DeleteClipRequest } from "./dto/request/delete-clip.request.dto";
-import { FfmpegModule } from "src/ffmpeg/ffmpeg.module";
 import { FfmpegService } from "src/ffmpeg/ffmpeg.service";
-import { OsHelper } from "src/utils/os/os.helper";
-import { ConfigModule } from "@nestjs/config";
 import { ClipDto } from "./dto/clip.dto";
 import { RoomDto } from "src/room/dto/room.dto";
+import { OsModule } from "src/utils/os/os.module";
+import { Test, TestingModule } from "@nestjs/testing";
 
 describe("ClipService", () => {
   let service: ClipService;
-  let clipRepository: ClipRepository;
-  let roomRepository: RoomRepository;
-  let s3Repository: S3Repository;
+
+  const mockClipRepository: any = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    remove: jest.fn(),
+  };
+
+  const mockRoomRepository: any = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    remove: jest.fn(),
+    addClip: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [S3Module, HashModule, FfmpegModule, ConfigModule],
+      imports: [OsModule],
       providers: [
         ClipService,
-        FfmpegService,
-        ClipRepository,
-        RoomRepository,
         {
-          provide: getModelToken(Room.name),
-          useFactory: () => {},
+          provide: ClipRepository,
+          useValue: mockClipRepository,
         },
         {
-          provide: getModelToken(Clip.name),
-          useFactory: () => {},
+          provide: RoomRepository,
+          useValue: mockRoomRepository,
         },
-        OsHelper,
+        {
+          provide: S3Repository,
+          useValue: {
+            uploadFile: jest
+              .fn()
+              .mockResolvedValue(Promise.resolve("video_url")),
+            getPresignedUrl: jest
+              .fn()
+              .mockResolvedValue(Promise.resolve("video_url")),
+          },
+        },
+        {
+          provide: FfmpegService,
+          useValue: {
+            makeWebmFile: jest.fn().mockResolvedValue(Promise.resolve(true)),
+          },
+        },
       ],
     }).compile();
-
-    clipRepository = module.get<ClipRepository>(ClipRepository);
-    roomRepository = module.get<RoomRepository>(RoomRepository);
-    s3Repository = module.get<S3Repository>(S3Repository);
-    const ffmpegService = module.get<FfmpegService>(FfmpegService);
-
-    jest
-      .spyOn(ffmpegService, "makeWebmFile")
-      .mockResolvedValue(Promise.resolve(true));
-
     service = module.get<ClipService>(ClipService);
   });
 
@@ -64,7 +72,7 @@ describe("ClipService", () => {
     it("조회된 데이터는 array 타입 ", async () => {
       // Arrange
       jest
-        .spyOn(clipRepository, "findAll")
+        .spyOn(mockClipRepository, "findAll")
         .mockResolvedValue(Promise.resolve([]));
 
       // Act
@@ -92,16 +100,10 @@ describe("ClipService", () => {
         ""
       );
       jest
-        .spyOn(s3Repository, "uploadFile")
-        .mockResolvedValue(Promise.resolve("video_url"));
-      jest
-        .spyOn(s3Repository, "getPresignedUrl")
-        .mockResolvedValue(Promise.resolve("video_url"));
-      jest
-        .spyOn(clipRepository, "create")
+        .spyOn(mockClipRepository, "create")
         .mockResolvedValue(Promise.resolve(mockClipDto));
       jest
-        .spyOn(roomRepository, "addClip")
+        .spyOn(mockRoomRepository, "addClip")
         .mockResolvedValue(
           Promise.resolve(new RoomDto("", "", "", "", null, []))
         );
@@ -124,14 +126,14 @@ describe("ClipService", () => {
       const password = "password";
       const input = new DeleteClipRequest(password);
 
-      jest.spyOn(clipRepository, "findOne").mockImplementation((id) => {
+      jest.spyOn(mockClipRepository, "findOne").mockImplementation((id) => {
         return id == clipId
           ? Promise.resolve({ password })
           : Promise.resolve(null);
       });
 
       jest
-        .spyOn(clipRepository, "remove")
+        .spyOn(mockClipRepository, "remove")
         .mockResolvedValue(Promise.resolve(true));
 
       // Act
