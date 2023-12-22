fdb23a86398ea11b8ae2f966c821e1a3d8d6dc8c
Heesung Youn <heesung6701@naver.com>
2023-12-22 19:57:50 +0900

    RoomRepository 테스트 간소화

--- src/room/room.controller.spec.ts
+++ src/room/room.controller.spec.ts
@@ -1,12 +1,5 @@
 import { HttpException, HttpStatus } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { getModelToken, MongooseModule } from "@nestjs/mongoose";
 import { Test, TestingModule } from "@nestjs/testing";
-import { S3Module } from "src/aws/s3/s3.module";
-import { GatheringModule } from "src/gathering/gathering.module";
-import { Room } from "src/schema/rooms.schema";
-import { HashHelper } from "src/utils/hash/hash.helper";
-import { HashModule } from "src/utils/hash/hash.module";
 import { ResponseMessage } from "src/utils/message.ko";
 import { OsModule } from "src/utils/os/os.module";
 import { CreateRoomResponse } from "./dto/response/create-room.response.dto";
@@ -14,81 +7,40 @@ import { CreateRoomRequest } from "./dto/request/create-room.request.dto";
 import { RoomResponse } from "./dto/response/room.response.dto";
 import { RoomDto } from "./dto/room.dto";
 import { RoomController } from "./room.controller";
-import { RoomRepository } from "./room.repository";
 import { RoomService } from "./room.service";
-import { AuthModule } from "src/auth/auth.module";
 import { AuthService } from "src/auth/auth.service";
-import {
-  Configuration,
-  ConfigurationSchema,
-} from "src/schema/configuration.schema";
-import { Clip } from "src/schema/clips.schema";
-import { ClipRepository } from "src/clip/clip.repository";
 
 describe("RoomController", () => {
   let controller: RoomController;
-  let service: RoomService;
-  let hashHelper: HashHelper;
+
+  const mockRoomService: any = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    remove: jest.fn(),
+    update: jest.fn(),
+    gather: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          isGlobal: true,
-        }),
-        MongooseModule.forRootAsync({
-          useFactory: (config: ConfigService) => ({
-            uri: config.get("MONGODB_URL").replace("${NODE_ENV}", "test"),
-          }),
-          inject: [ConfigService],
-        }),
-        MongooseModule.forFeatureAsync([
-          {
-            name: Configuration.name,
-            useFactory: () => {
-              return ConfigurationSchema;
-            },
-          },
-        ]),
-        HashModule,
-        OsModule,
-        GatheringModule,
-        S3Module,
-        AuthModule,
-      ],
+      imports: [OsModule],
       controllers: [RoomController],
       providers: [
-        RoomService,
-        RoomRepository,
-        ClipRepository,
         {
-          provide: getModelToken(Room.name),
-          useFactory: () => {},
+          provide: RoomService,
+          useValue: mockRoomService,
         },
         {
-          provide: getModelToken(Clip.name),
-          useFactory: () => {},
+          provide: AuthService,
+          useValue: {
+            isKeyValid: jest.fn().mockResolvedValue(true),
+          },
         },
       ],
     }).compile();
 
     controller = module.get<RoomController>(RoomController);
-    service = module.get<RoomService>(RoomService);
-    hashHelper = module.get<HashHelper>(HashHelper);
-    const authService: AuthService = module.get<AuthService>(AuthService);
-
-    jest
-      .spyOn(hashHelper, "createHash")
-      .mockImplementation(async (password: string) => {
-        return password;
-      });
-
-    jest
-      .spyOn(hashHelper, "isMatch")
-      .mockImplementation(async (password: string, hash: string) => {
-        return password == hash;
-      });
-    jest.spyOn(authService, "isKeyValid").mockResolvedValue(true);
   });
 
   it("should be defined", () => {
@@ -99,7 +51,7 @@ describe("RoomController", () => {
     it("성공 케이스 ", async () => {
       // Arrange
       jest
-        .spyOn(service, "create")
+        .spyOn(mockRoomService, "create")
         .mockResolvedValue(
           Promise.resolve(new RoomDto("", "", "", "", new Date(), []))
         );
@@ -118,11 +70,11 @@ describe("RoomController", () => {
       const password = "password";
 
       jest
-        .spyOn(service, "create")
+        .spyOn(mockRoomService, "create")
         .mockResolvedValue(
           Promise.resolve(new RoomDto(roomId, "", password, "", new Date(), []))
         );
-      jest.spyOn(service, "findOne").mockImplementation((id) => {
+      jest.spyOn(mockRoomService, "findOne").mockImplementation((id) => {
         return Promise.resolve(
           id == roomId
             ? new RoomDto(roomId, "", password, "", new Date(), [])
