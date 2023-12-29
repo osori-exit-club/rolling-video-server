74ce8c5ab1b7fc8af06bb735bdf42a959d752c10
윤희성 <heesung6701@naver.com>
2023-12-29 23:25:21 +0900

    add mutext for grathering

--- src/domain/room/feature/gathering/gathering.service.ts
+++ src/domain/room/feature/gathering/gathering.service.ts
@@ -3,9 +3,11 @@ import * as path from "path";
 import { Injectable } from "@nestjs/common";
 import { S3Repository } from "src/shared/aws/s3/s3.repository";
 import { CompressHelper } from "src/domain/room/feature/compress/comporess.helper";
+import { Mutex } from "async-mutex";
 
 @Injectable()
 export class GatheringService {
+  private mutex = new Mutex();
   constructor(
     private readonly s3Repository: S3Repository,
     private readonly compressHelper: CompressHelper
@@ -17,14 +19,15 @@ export class GatheringService {
     downloadDir: string,
     outFilePath: string
   ) {
+    const release = await this.mutex.acquire();
     await Promise.all(
       s3PathList.map((key: string) => {
         this.s3Repository.download(key, downloadDir);
       })
     );
     await this.compressHelper.compress(downloadDir, outFilePath);
+    release();
     const fileContent = fs.readFileSync(outFilePath);
-
     return await this.s3Repository.uploadFile({
       buffer: fileContent,
       key: s3key,
